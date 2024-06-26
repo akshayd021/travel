796ff69932893f1e0473abf1f3d0496f49a0f10e
akshayd021 <akshaydholakiya69@gmail.com>
2024-06-26 08:42:36 +0530

    fix

---
+++ app/componets/index.js
@@ -0,0 +1,10 @@
+import Button from "./Button";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Camp from  './Camp'
+import Guide from "./Guide";
+import Features from "./Features";
+import GetApp from "./GetApp";
+
+export { Button, Footer, Navbar , Hero, Camp, Guide, Features, GetApp};
